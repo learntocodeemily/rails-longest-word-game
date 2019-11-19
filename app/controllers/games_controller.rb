b85483f807f4f70a8277b68c9e99bed16cab12f1
liting zhao <emily51905@163.com>
2019-11-19 09:05:38 +0800

    not finished,before calculate the score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+end
